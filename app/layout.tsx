c6de8bdc7aac8b23f1d6195b900bac1c2d5f8ecb
prabuddhaxdev <prabuddha1749@gmail.com>
2025-09-02 22:36:40 +0530

    Add Mouse Moving Effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter } from "next/font/google";
 import "./globals.css";
+import MouseMoveEffect from "@/components/ui/mouse-move-effect";
 
 const inter=Inter({subsets:['latin']});
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
+        <MouseMoveEffect />
         {children}
       </body>
     </html>
